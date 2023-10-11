9a1619fc5f8b1f94c05b5e651cd1ba9fdb133749
Md Shaquib Alam <101167802+shaquib1@users.noreply.github.com>
2023-10-11 21:47:23 +0530

    Create Binary_search.cpp
    implementation of binary search in cpp

---
+++ Binary_search.cpp
@@ -0,0 +1,48 @@
+
+// Binary search implementaiton in C++
+
+#include <iostream>
+
+using namespace std;
+
+int binaryseach(int arr[], int size, int key)
+{
+
+    int start = 0;
+    int end = size - 1;
+
+    int mid = (start + end) / 2; // start + (end-start)/2
+
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+
+        // go to right part
+        if (key > arr[mid])    
+        {
+            start = mid + 1; 
+        }
+        else   //key less than mid than go to left part
+        {
+            end = mid - 1;
+        }
+
+        mid = (start + end) / 2;  // start + (end-start)/2
+    }
+
+    return -1;
+}
+
+int main()
+{
+
+    int arr[5] = {4, 8, 9, 14, 25};
+
+    int findIndex = binaryseach(arr, 5, 25);
+    cout << "index of key is : " << findIndex << endl;
+
+    return 0;
+};
